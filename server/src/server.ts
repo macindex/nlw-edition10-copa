686a5485aa16f8a560686be106b0dac91c394cf1
macindex <machollanda@gmail.com>
2022-11-12 23:46:04 -0300

    create new pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { z } from 'zod'
+import sui from 'short-unique-id'
 
 const prisma = new PrismaClient({ 
     log: ['query'],
@@ -16,10 +18,25 @@ async function bootstrap() {
         origin: true,
     })
 
-    fastify.get('/pools/count', async () => {
-        const count = await prisma.pool.count()
+    fastify.post('/pools', async (request, reply) => {
         
-        return { count }
+        const createPoolBody = z.object({
+            title: z.string(),
+        })
+        
+        const { title } = createPoolBody.parse(request.body)
+
+        const generate = new sui({ length: 6 })
+        const code = String(generate()).toUpperCase()
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+        
+        return reply.status(201).send({ code })
+        // return { title }
     })
     await fastify.listen({ port: 3333 })
     // await fastify.listen({ port: 3333, host: '0.0.0.0' })
